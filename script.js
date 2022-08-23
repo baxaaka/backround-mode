431b40c063a88ef327edbb90965a850b5d57245c
baxaaka <jockuhacker@gmail.com>
2022-08-23 23:01:25 +0500

    svitch on/of

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+
+
+
+
+function darkMode (){
+   document.getElementById("body-2").style.backgroundColor ="black"
+};
+
+function lightMode (){
+    document.getElementById("body-2").style.backgroundColor ="#F7ECDE"
+}
